3736a28a4e3307b072dfa265cbdaa73f21ca5ca8
shashi7846 <73377472+shashi7846@users.noreply.github.com>
2021-06-20 19:46:12 +0530

    Update nav.js

--- src/nav.js
+++ src/nav.js
@@ -11,7 +11,7 @@ function Nav() {
     return (
         <>
             <nav class="navbar navbar-expand-lg navbar-light bg-dark">
-                <a class="navbar-brand text-light">DOMAINCER HIRING</a>
+                <a class="navbar-brand text-light">HIRING PORTAL</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
